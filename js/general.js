ceb48dd6af8a946de5a6f477bc77c4f4acb9723b
mathiasiriarte <mathias.iriarte@hotmail.com>
2021-04-27 14:35:39 -0300

    Added selectors

---
+++ js/general.js
@@ -0,0 +1,37 @@
+//---------- NAV ----------
+const section_nav = document.querySelector(".nav");
+const logo = document.querySelector(".nav-logo");
+const search_nav = document.querySelector(".nav-search-container");
+const searchBar_nav = document.querySelector(".nav-search-bar");
+const nav_purple_magnifyingGlass = document.querySelector(".nav-searchIcon-purple");
+const nav_gray_magnifyingGlass = document.querySelector(".nav-searchIcon-gray");
+const nav_search_close = document.querySelector(".nav-search-close");
+const hamburguer = document.querySelector(".nav-container-burger");
+const ul_nav = document.querySelector(".nav-container-items");
+const ul_li_nav = document.querySelectorAll(".nav-container-items li");
+//---------- LINKS ----------
+const darkMode_toggle = document.getElementById("btnDarkMode");
+const favorites_link = document.getElementById("btnFav");
+const myGifs_link = document.getElementById("btnMyGifs");
+//---------- SEARCH SECTION ----------
+const search_section = document.querySelector(".search-section");
+const popular_tags = document.querySelector(".search-trending-tags");
+const input_search = document.querySelector(".searchbar");
+const searchPurple = document.querySelector(".search-icon-purple");
+const searchGray = document.querySelector(".search-icon-gray");
+const cancel_search = document.querySelector(".search-close");
+const autocomplete_suggest = document.querySelector(".autocomplete");
+//--------------------
+const maximizeGif_section = document.querySelector(".max");
+const favoriteGifs_section = document.querySelector(".favs");
+const myGifs_section = document.querySelector(".my-gifs");
+const results_search = document.querySelector(".search-results");
+//---------- TRENDING SECTION ----------
+const trendingGifs_section = document.querySelector(".trending-gifos");
+const trendingGifs_container = document.querySelector(".trending-gifos-gifos-container");
+const trendingRightArrow = document.getElementById("rightArrow");
+const trendingLeftArrow = document.getElementById("leftArrow");
+//---------- FOOTER ----------
+const facebook = document.querySelectorAll(".share-buttons")[0];
+const twitter = document.querySelectorAll(".share-buttons")[1];
+const instagram = document.querySelectorAll(".share-buttons")[2];
