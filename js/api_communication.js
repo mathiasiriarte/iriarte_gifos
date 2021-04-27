2a30a4cf85382ea2540cda532565f25b34cbdef1
mathiasiriarte <mathias.iriarte@hotmail.com>
2021-04-27 12:24:22 -0300

    Get trending search tags function

--- js/api_communication.js
+++ js/api_communication.js
@@ -6,3 +6,14 @@ const api_search_endpoint = "https://api.giphy.com/v1/gifs/search";
 const api_trendingGif_endpoint = "https://api.giphy.com/v1/gifs/trending";
 //----- POPULAR SEARCH TAGS -----
 const api_trendingSearchTags_endpoint = "https://api.giphy.com/v1/trending/searches";
+
+
+//---------- CALL TRENDING SEARCH TAGS ----------
+async function trendingSearchTags() {
+    await fetch(api_trendingSearchTags_endpoint + "api_key=" + generated_api_key)
+        .then(response => { return (response.json()) })
+        .then(json => {
+            console.log(json);
+        })
+        .catch(err => console.log(err))
+}
