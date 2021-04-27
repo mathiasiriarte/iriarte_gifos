6a954fc8bd435999c8925874edb7e8e7a1c270c6
mathiasiriarte <mathias.iriarte@hotmail.com>
2021-04-27 12:12:47 -0300

    Added endpoints on api js and styles on general scss

--- js/api_communication.js
+++ js/api_communication.js
@@ -1,1 +1,8 @@
+//----- KEY -----
 const generated_api_key = "dEU71xYON78mNX3VJOtvuScHXSKxkWDI";
+//----- SEARCH -----
+const api_search_endpoint = "https://api.giphy.com/v1/gifs/search";
+//----- TRENDING GIFS -----
+const api_trendingGif_endpoint = "https://api.giphy.com/v1/gifs/trending";
+//----- POPULAR SEARCH TAGS -----
+const api_trendingSearchTags_endpoint = "https://api.giphy.com/v1/trending/searches";
